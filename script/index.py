76051f0d8151a16f9fcb87bf9ed1a8871060600f
baltth <baltth@gmail.com>
2025-11-21 01:25:32 +0100

    Index: added comets and asterism to 'Other' category, split 'Moon'

--- script/index.py
+++ script/index.py
@@ -8,7 +8,7 @@ import pages
 from collections import OrderedDict
 from natsort import natsorted
 from operator import itemgetter
-from typing import Callable, Dict, List
+from typing import Callable, Dict, List, Iterable
 
 
 def raw_data(obs_db: List[ObsData], object_db: Dict[str, Object]) -> List:
@@ -27,6 +27,30 @@ def raw_data(obs_db: List[ObsData], object_db: Dict[str, Object]) -> List:
     return natsorted(data.values(), key=itemgetter('name'))
 
 
+def get_type(obj: Object) -> str:
+    type_split = obj.type.split()
+
+    if 'star' in type_split:
+        return 'Stars'
+    if obj.constellation == 'Moon' \
+        or 'crater' in type_split:  # Yes, a 'crater' is assumed to be on Moon...
+        return 'Moon'
+    if 'planet' in type_split \
+        or 'comet' in type_split \
+        or 'asterism' in type_split:
+        return 'Other'
+    return 'Deep space'
+
+
+def sort_categories(data: Iterable) -> OrderedDict:
+    res = OrderedDict(natsorted(data))
+    if 'Moon' in res.keys():
+        res.move_to_end('Moon')
+    if 'Other' in res.keys():
+        res.move_to_end('Other')
+    return res
+
+
 def collect(data: List[Dict], key: Callable) -> OrderedDict[str, List[Dict]]:
 
     res = {}
@@ -40,10 +64,7 @@ def collect(data: List[Dict], key: Callable) -> OrderedDict[str, List[Dict]]:
     for v in res.values():
         v = natsorted(v)
 
-    res = OrderedDict(natsorted(res.items()))
-    if 'Other' in res.keys():
-        res.move_to_end('Other')
-    return res
+    return sort_categories(res.items())
 
 
 def subpage(title: str, content: List[str]) -> List[str]:
@@ -51,24 +72,18 @@ def subpage(title: str, content: List[str]) -> List[str]:
     return [pages.subtitle(title), ''] + content + ['']
 
 
+
 def index_content(obs_db: Dict, object_db: Dict[str, Object]) -> List[str]:
 
     raw = raw_data(obs_db, object_db)
 
-    def by_type(obj: Object) -> str:
-        # todo
-        if 'star' in obj.type:
-            return 'Stars'
-        if 'planet' in obj.type.split() or 'crater' in obj.type:
-            return 'Other'
-        return 'Deep space'
 
     def by_constellation(obj: Object) -> str:
         if constellations.is_constellation(obj.constellation):
             return constellations.name(obj.constellation)
         return 'Other'
 
-    index_by_cat = pages.index_data(collect(raw, key=by_type))
+    index_by_cat = pages.index_data(collect(raw, key=get_type))
     index_by_const = pages.index_data(collect(raw, key=by_constellation))
 
     return subpage('Categories', index_by_cat) + subpage('By constellation', index_by_const)
